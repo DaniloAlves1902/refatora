e02cfcc81cb9e6c75f982a6302dd7b83b815cd75
DaniloAlves1902 <danilo.alvess1902@gmail.com>
2026-02-02 08:04:14 -0300

    feat: add UpdateUserDTO

---
+++ src/main/java/br/com/alvesdanilo/refatora/modules/user/dto/UpdateUserDTO.java
@@ -0,0 +1,28 @@
+package br.com.alvesdanilo.refatora.modules.user.dto;
+
+import br.com.alvesdanilo.refatora.modules.user.model.SeniorityLevel;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record UpdateUserDTO(
+        @NotBlank(message = "First name is required")
+        String firstName,
+
+        @NotBlank(message = "Last name is required")
+        String lastName,
+
+        @NotBlank(message = "Username is required")
+        @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+        String username,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email,
+
+        @NotNull(message = "Seniority level is required")
+        SeniorityLevel seniorityLevel
+) {
+
+}
