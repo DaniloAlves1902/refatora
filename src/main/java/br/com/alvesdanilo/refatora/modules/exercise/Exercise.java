9f8863423006421f32333458342799964974e7b5
DaniloAlves1902 <danilo.alvess1902@gmail.com>
2026-02-13 10:24:21 -0300

    feat: add starterCode and technology fields to Exercise entity

--- src/main/java/br/com/alvesdanilo/refatora/modules/exercise/Exercise.java
+++ src/main/java/br/com/alvesdanilo/refatora/modules/exercise/Exercise.java
@@ -28,9 +28,18 @@ public class Exercise {
     @Column(nullable = false, length = 2000)
     private String description;
 
+    @Column(columnDefinition = "TEXT")
+    private String starterCode;
+
+    @Column(nullable = false, unique = true)
+    private String slug;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private SeniorityLevel difficulty;
+
+    @Column(nullable = false, length = 50)
+    private String technology;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "author_id", nullable = false)
